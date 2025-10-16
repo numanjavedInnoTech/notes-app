514284920d148842dfcd203fa98664f36404c094
numaanjaved <naumaan.javed@hotmail.com>
2025-10-16 14:40:13 +0500

    created noteform and noteslist components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
+import NoteForm from "./components/NoteForm";
+import NotesList from "./components/NotesList";
+
 const App = () => {
+  const [notes, setNotes] = useState([]);
   return ( 
     <div className="max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg">
       <h2 className="text-2xl font-bold mb-4 text-center">Notes App</h2>
+      <NoteForm notes={notes} setNotes={setNotes}/>
+      <NotesList notes={notes} />
     </div>
    );
 }
---
+++ src/components/NoteForm.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+
+const NoteForm = ({ notes, setNotes }) => {
+    const [formData, setFormData] = useState({
+        title: '',
+        priority: 'Medium',
+        category: 'Work',
+        description: ''
+    });
+    const [isFormVisible, setIsFormVisible] = useState(false);
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(!formData.title || !formData.description) return;
+        const newNote = { id: Date.now(), ...formData}
+        setNotes([newNote, ...notes]);
+        setFormData({
+            title: '',
+            priority: 'Medium',
+            category: 'Work',
+            description: ''
+        })
+    }
+    const formButton = <button onClick={(e) => {setIsFormVisible(!isFormVisible)}}
+        className="w-full bg-gray-100 border border-gray-300 text-purple-800 py-2 rounded-lg cursor-pointer hover: bg-purple-200 hover: border-purple-300 transition mb4" >
+        {isFormVisible  ? 'Hide Form x' : 'Add New Note +' }
+    </button>
+    const formNote = <form onSubmit={handleSubmit} action="" className="mb-6">
+        <div className="mb4">
+            <label htmlFor="title" className="block font-semibold">Title</label>
+            <input type="text" className="w-full p2 border rounded-lg" name="title" value={formData.title} 
+            onChange={handleChange} />
+        </div>
+        <div className="mb4">
+            <label htmlFor="priority" className="block font-semibold">Priority</label>
+            <select type="text" className="w-full p2 border rounded-lg" name="priority" value={formData.priority} 
+            onChange={handleChange}>
+                <option value="High">High</option>
+                <option value="Medium">Medium</option>
+                <option value="Low">Low</option>
+            </select>
+        </div>
+        <div className="mb4">
+            <label htmlFor="category" className="block font-semibold">Category</label>
+            <select type="text" className="w-full p2 border rounded-lg" name="category" value={formData.category} 
+            onChange={handleChange}>
+                <option value="Work">Work</option>
+                <option value="Personal">Personal</option>
+                <option value="Ideas">Ideas</option>
+            </select>
+        </div>
+        <div className="mb4">
+            <label htmlFor="description" className="block font-semibold">Description</label>
+            <textarea type="text" className="w-full p2 border rounded-lg" name="description" value={formData.description} 
+            onChange={handleChange}></textarea>
+        </div>
+        <button className="w-full bg-purple-500 text-white py-2 rounded-lg cursor-pointer hover: bg-purple-600">
+            Add Note
+        </button>
+    </form>;
+    return ( 
+        <>
+            {formButton}
+            {isFormVisible && formNote}
+        </>
+        
+     );
+}
+ 
+export default NoteForm;
